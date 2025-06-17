d1b8331d5c492b5c87bcc080501b6e85a1fbb0d9
Fenil-rokad <fenilrokad3@gmail.com>
2025-06-17 21:31:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your code here
+const input = document.getElementById("evaluatedText");
+const letterCount = document.getElementById("letterCount");
+
+input.addEventListener("input", function () {
+  letterCount.textContent = input.value.length;
+});
